31f36489dfb78bf1cd2af612a0e80912ac2fe750
liliana579 <68746742+liliana579@users.noreply.github.com>
2020-09-05 23:04:01 +0300

    Add files via upload

---
+++ js/jquery-func.js
@@ -0,0 +1,29 @@
+$( document ).ready( function(){
+	$('.blink')
+		.focus(function(){
+			if( $(this).attr('value') == $(this).attr('title') ) {
+				$(this).attr({ 'value': '' });
+			}
+		})
+		.blur(function(){
+			if( $(this).attr('value') == '' ) {
+				$(this).attr({ 'value': $(this).attr('title') })
+			}
+		});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
